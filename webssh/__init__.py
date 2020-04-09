b1847c0cb7a34da3ed95a780e81e66e28ed07acd
zhangxingbo <215957956@qq.com>
2020-04-09 15:12:04 +0800

    zxb

--- webssh/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'webssh.apps.WebsshConfig'
--- webssh/routing.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import consumers
-
-websocket_urlpatterns = [
-    url('ws/webssh/<int:pk>', consumers.WebsshConsumer),
-    url('ws/webssh/', consumers.WebsshConsumer),
-]
