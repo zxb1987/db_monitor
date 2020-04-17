91e2a0e4853c4f985bfb2587d040949f166091fe
xiaowu94520 <2515543601@qq.com>
2020-04-17 18:03:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	role/views.py

--- role/urls.py
+++ role/urls.py
@@ -7,5 +7,5 @@ app_name = "role"
 urlpatterns = [
     path('api/role', views.ApiRoleList.as_view()),
     path('api/role/<int:pk>', views.ApiRoleDetail.as_view()),
-    path('api/role/page=<int:pk>', views.PageLimit),
+    path('api/role/page=<int:pk>', views.PageInfo),
 ]
