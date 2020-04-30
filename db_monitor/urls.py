9e3a4ed96729f5ea46c720edbaeeb1e0b42b1c2e
xiaowu94520 <2515543601@qq.com>
2020-04-30 17:43:44 +0800

    111
    Signed-off-by: xiaowu94520 <2515543601@qq.com>

--- db_monitor/urls.py
+++ db_monitor/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
-from maintaintools import tests
-from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
